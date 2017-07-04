f9b1ece8d173c71d81329ff685be137f41abd63e
Aaroh Mankad <aarohmankad@gmail.com>
2017-07-04 15:50:03 -0700

    Using basic sweetalert, user can save times

--- index.js
+++ index.js
@@ -3,20 +3,14 @@ let
 	break_time = document.querySelector('#break_time').value;
 
 document.querySelector('.btn-success').addEventListener('click', () => {
-	work_time = document.querySelector('#work_time').value;
-	break_time = document.querySelector('#break_time').value;
+	work_time = document.querySelector('#work_time').value * 60000;
+	break_time = document.querySelector('#break_time').value * 1000;
 
 	chrome.runtime.sendMessage({
 		state: 'UPDATE',
 		work_time,
 		break_time,
-	}, (response) => {});
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  switch (message.state) {
-    case 'INITIAL':
-      alert(message.message);
-      break;
-  }
+	}, (response) => {
+		window.close();
+	});
 });
